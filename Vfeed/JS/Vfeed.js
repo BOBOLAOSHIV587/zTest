eef4bb7d842c519f702727d5e64d47cfbfca25b3
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-17 01:49:12 +0800

    Update Vfeed.js

--- Vfeed/JS/Vfeed.js
+++ Vfeed/JS/Vfeed.js
@@ -1,15 +1,33 @@
 let body = JSON.parse($response.body);
-body.me.active_bundle_subscriptions = [
-  {
-    "features": [
-
+body = {
+  "code": 0,
+  "data": {
+    "inApp": [
     ],
-    "expiry": "2099-12-31T23:59:59+00:00",
-    "product_id": "com.focoslive.lifetime",
-    "plan_id": null
-  }
-]
-body.me.active_subscriptions_ids = [
-  "com.focoslive.lifetime"
-]
+    "latestReceiptInfo": [
+      {
+        "productId": "vmake.subs.month12.func00.lev00.2023trial.ver1",
+        "quantity": 1,
+        "promotionalOfferId": "",
+        "purchaseDateMs": "1707507158000",
+        "autoRenewStatus": true,
+        "productType": "",
+        "originalTransactionId": "470001823122615",
+        "expiresDateMs": "4102444799000",
+        "subscriptionGroupIdentifier": "20897211",
+        "transactionId": "470001823122615",
+        "offerCodeRefName": "",
+        "awTransId": "3d4cbaa7-e415-4862-b6c3-ff3781a0deab",
+        "isTrialPeriod": true,
+        "gracePeriodExpiresDateMs": "",
+        "isInIntroOfferPeriod": false,
+        "originalPurchaseDateMs": "1707507159000",
+        "inAppOwnershipType": "PURCHASED",
+        "status": 1
+      }
+    ]
+  },
+  "message": "success",
+  "update": ""
+}
 $done({body: JSON.stringify(body)});
