da99dc101e17941f688cc11ba104d32e7c81cf3c
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-17 01:33:41 +0800

    Update Vfeed.js

--- Vfeed/JS/Vfeed.js
+++ Vfeed/JS/Vfeed.js
@@ -1,25 +1,15 @@
 let body = JSON.parse($response.body);
-body.data.results.subscriptions =
-[
+body.me.active_bundle_subscriptions = [
   {
-    "id": "5714032c-dc15-46b9-a476-be497786da9a",
-    "unit": "month",
-    "group_id": "fbc1128b",
-    "autorenew_enabled": true,
-    "expires_at": "2099-12-31T23:59:59.000Z",
-    "in_retry_billing": false,
-    "introductory_activated": true,
-    "cancelled_at": null,
-    "platform": "ios",
-    "product_id": "ru.bestfeeds.vkfeed.subscribeOneMonth",
-    "retries_count": 0,
-    "started_at": "2023-07-05T13:38:50.000Z",
-    "local": false,
-    "next_check_at": "2099-12-31T23:59:59.000Z",
-    "kind": "autorenewable",
-    "units_count": 1,
-    "environment": "production",
-    "status": "trial"
+    "features": [
+
+    ],
+    "expiry": "2099-12-31T23:59:59+00:00",
+    "product_id": "com.focoslive.lifetime",
+    "plan_id": null
   }
+]
+body.me.active_subscriptions_ids = [
+  "com.focoslive.lifetime"
 ]
 $done({body: JSON.stringify(body)});
