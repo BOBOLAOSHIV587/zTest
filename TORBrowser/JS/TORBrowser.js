a28fbb9e1c4a704796ac25bdc2c6431c98199207
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-17 00:11:52 +0800

    Update TORBrowser.js

--- TORBrowser/JS/TORBrowser.js
+++ TORBrowser/JS/TORBrowser.js
@@ -1,8 +1,4 @@
 let body = JSON.parse($response.body)
-body = {
-    "status": "ok",
-    "payload": {
-        "active": true
-    }
-}
+body.isPro = true
+body.expires = 4102444799000
 $done({body: JSON.stringify(body)})
