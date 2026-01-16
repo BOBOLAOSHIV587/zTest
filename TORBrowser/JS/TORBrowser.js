dd6e8c324c461ad0c1272b329944d1d2560d042d
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-17 02:22:02 +0800

    Update TORBrowser.js

--- TORBrowser/JS/TORBrowser.js
+++ TORBrowser/JS/TORBrowser.js
@@ -1,23 +1,22 @@
-let body = JSON.parse($response.body);
-body.data.list = [
-  {
-    "autoRenewProductId": "VivaCut_Yearly_Pro_9",
-    "orderId": "470001817417189",
-    "isTrialPeriod": false,
-    "endTime": 4102444799000,
-    "productId": "VivaCut_Yearly_Pro_9",
-    "productType": 3,
-    "startTime": 1707126877000,
-    "orderStatus": 1,
-    "autoRenewStatus": true,
-    "originalOrderId": "470001817417189",
-    "sign": "de523bfabb4dc6150e8dfbaa10e67f20"
+let body = JSON.parse($response.body)
+body = {
+  "riveo": {
+    "android": {
+      "riveo.features.premium": {
+        "expire_at": "2099-09-09T20:20:20.000Z",
+        "id": "riveo.features.premium",
+        "expire_at_ts": 4092657620000,
+        "is_active": true
+      }
+    },
+    "ios": {
+      "riveo.features.premium": {
+        "expire_at": "2099-09-09T20:20:20.000Z",
+        "id": "riveo.features.premium",
+        "expire_at_ts": 4092657620000,
+        "is_active": true
+      }
+    }
   }
-]
-body.data.hasFreedTrialProds = [
-  "VivaCut_Yearly_Pro_9"
-]
-body.data.hasPurchasedProds = [
-  "VivaCut_Yearly_Pro_9"
-]
-$done({body: JSON.stringify(body)});
+}
+$done({body: JSON.stringify(body)})
