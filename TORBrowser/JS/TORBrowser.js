fd715885fdd70703918b57abae8dd5d7551f5264
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-17 02:41:41 +0800

    Update TORBrowser.js

--- TORBrowser/JS/TORBrowser.js
+++ TORBrowser/JS/TORBrowser.js
@@ -1,22 +1,67 @@
-let body = JSON.parse($response.body)
-body = {
-  "riveo": {
-    "android": {
-      "riveo.features.premium": {
-        "expire_at": "2099-09-09T20:20:20.000Z",
-        "id": "riveo.features.premium",
-        "expire_at_ts": 4092657620000,
-        "is_active": true
+if($request.url.includes("order/restore")) {
+body = body = {
+  "code": 0,
+  "message": "success",
+  "data": {
+    "inApp": [],
+    "latestReceiptInfo": [
+      {
+        "quantity": 1,
+        "productId": "airbrush.subs.mongth12.func00.lev00.standard.ver2",
+        "promotionalOfferId": "",
+        "purchaseDateMs": "1717328288000",
+        "autoRenewStatus": true,
+        "productType": "",
+        "originalTransactionId": "1470000008834448",
+        "expiresDateMs": "4102444799000",
+        "subscriptionGroupIdentifier": "21417206",
+        "transactionId": "1470000008834448",
+        "offerCodeRefName": "",
+        "awTransId": "30100000-2024-0602-1137-069894793197",
+        "isTrialPeriod": false,
+        "gracePeriodExpiresDateMs": "",
+        "isInIntroOfferPeriod": false,
+        "originalPurchaseDateMs": "1717328288000",
+        "inAppOwnershipType": "PURCHASED",
+        "status": 2
       }
-    },
-    "ios": {
-      "riveo.features.premium": {
-        "expire_at": "2099-09-09T20:20:20.000Z",
-        "id": "riveo.features.premium",
-        "expire_at_ts": 4092657620000,
-        "is_active": true
-      }
-    }
-  }
+    ]
+  },
+  "update": ""
 }
-$done({body: JSON.stringify(body)})
+}
+else {
+body = {
+  "message": "success",
+  "data": {
+    "entitlements": [
+      {
+        "purchaseDateMs": "1717328288000",
+        "transaction_id": "1470000008834448",
+        "grace_period_expires_date_ms": "0",
+        "start_time": "",
+        "type": 3,
+        "key": "subscription_iOS",
+        "is_trial_period": false,
+        "original_transaction_id": "1470000008834448",
+        "product_id": "airbrush.subs.mongth12.func00.lev00.standard.ver2",
+        "items": [
+          "Whiten",
+          "Brighten",
+          "Auto-retouch",
+          "Dark Circles",
+          "Acne",
+          "Smooth"
+        ],
+        "end_time": "",
+        "originalPurchaseDateMs": "1717328288000",
+        "product_type": 2,
+        "expires_date_ms": "4102444799000",
+        "product_cate": 1
+      }
+    ]
+  },
+  "code": 0
+}
+}
+$done({body: JSON.stringify(body)});
