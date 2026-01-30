7832da0a0867d827b1392bd1b25b0d25e651ca9e
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-31 02:49:58 +0800

    Update KG.js

--- KG.js
+++ KG.js
@@ -1,50 +1,14 @@
-(() => {
-  const { url } = $request;
-  const endpoints = [
-    {
-      match: "v1/payments/profiles/@me/subscription",
-      body: {
-        isSubscribed: true,
-        planId: "co.polarr.ppe.premium.studio.yearly",
-        subscriptionProduct: "yearly",
-        isTrial: false,
-        app: "PPE",
-        isUnlimited: true,
-        expiryDate: "2099-12-20T22:00:00.000Z",
-        planType: "yearly",
-        planTier: "studio",
-        startDate: "2023-12-20T22:00:00.000Z",
-        subscriptionTier: "studio",
-        paymentChannel: "AppleIapSubscription",
-        membershipExpiryDate: "2099-12-20T22:00:00.000Z"
-      }
-    },
-    {
-      match: "v1/payments/appleiap/receipts/confirmation",
-      body: {
-        app: "PPE",
-        planType: "yearly",
-        planTier: "studio",
-        isUnlimited: true,
-        membershipExpiryDate: "2099-12-20T22:00:00.000Z"
-      }
+let body = JSON.parse($response.body);
+body.data = {
+    "rights": {
+      "renewal": true,
+      "vip_type": "YEAR_PACKAGE_PLUS",
+      "vip_label": true,
+      "vip_remainder_day": 2,
+      "expires_date": 1738942908000,
+      "have_trial": true,
+      "expires_date_format": "2099-12-31 23:59:59.000",
+      "vip_product_id": "106"
     }
-  ];
-
-  try {
-    JSON.parse($response.body);
-  } catch {}
-
-  let responseBody = {};
-  for (const endpoint of endpoints) {
-    if (url.includes(endpoint.match)) {
-      responseBody = endpoint.body;
-      break;
-    }
-  }
-
-  $done({
-    status: 200,
-    body: JSON.stringify(responseBody)
-  });
-})();
+}
+$done({body: JSON.stringify(body)});
