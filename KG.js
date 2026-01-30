6fa25806b12882a8555a35c952c9f2f632142095
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-31 03:03:59 +0800

    Update KG.js

--- KG.js
+++ KG.js
@@ -4,36 +4,3 @@ body.hasPremium = true,
 body.subscriptionStartDate = "2023-11-03T11:09:34.929Z"
 console.log(body);
 $done({body: JSON.stringify(body)});
-
-*///
-const body = JSON.parse($response.body);
-if (body.data?.getSubscriptionSummary) {
-    body.data.getSubscriptionSummary.hasPremium = true;
-	body.data.getSubscriptionSummary.currentTier = "PREMIUM";
-	body.data.getSubscriptionSummary.products = [
-        {
-          "activeSubscriptionDetails": {
-            "frequencyInterval": 1,
-            "frequencyUnit": "YEAR"
-          },
-          "productId": "mfp_12m_ios_4999_1m_trial",
-          "subscriptionTier": "PREMIUM",
-          "subscriptionType": "TRIAL",
-          "requestedCancellationDate": "2025-02-13",
-          "paymentProvider": "APPLE",
-          "subscriptionEndDateTime": "2099-03-13T11:12:25.000Z",
-          "willRenew": false,
-          "subscriptionStartDateTime": "2099-02-13T12:12:25.000Z"
-        }
-      ];
-if (Array.isArray(body.data.getSubscriptionSummary.features)) {
-        body.data.getSubscriptionSummary.features.forEach(feature => {
-            if (feature.entitlement === "UPGRADABLE") {
-                feature.entitlement = "ENTITLED";
-            }
-        });
-    }
-$done({body: JSON.stringify(body)});
-} else {
-$done({});
-}
