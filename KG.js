f51a933dc447bc3bf5e81e044f3160a948b85311
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-05-30 02:31:12 +0800

    Update KG.js

--- KG.js
+++ KG.js
@@ -1,35 +1,12 @@
 /*
-[rewrite_local]
-^https?://(user|app|entry).qtfm.cn/(m-bff|api|u2/api)/(v1|v5)/(channel_verify|personal/?carrier|user).*$ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/zTest/refs/heads/main/KG.js
-^https?://app.qtfm.cn/m-bff/v1/audiostreams url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/zTest/refs/heads/main/KG.js
+# [rewrite_local]
+# ^https?://(user|app|entry).qtfm.cn/(m-bff|api|u2/api)/(v1|v5)/(channel_verify|personal/?carrier|user).*$ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/zTest/refs/heads/main/KG.js
+# ^https?://app.qtfm.cn/m-bff/v1/audiostreams url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/zTest/refs/heads/main/KG.js
 
-[mitm]
+# [mitm]
 /*
-/**
- * GitHub Web URL to Raw URL Converter
- * 适配 Quantumult X, Surge, Loon
- */
+^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/(checkCount|consumeCount) url jsonjq-response-body .data.totalCount = 999 | .data.currCount = 999
 
-const url = $request.url;
-// 匹配标准 GitHub 文件浏览链接
-// 示例: https://github.com/user/repo/blob/branch/path/to/file
-const githubRegex = /^https?:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+)$/;
+^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/createTtsAudio url jsonjq-response-body .data.freeCount = 999
 
-if (githubRegex.test(url)) {
-    const rawUrl = url.replace(githubRegex, 'https://raw.githubusercontent.com/$1/$2/$3/$4');
-    
-    // 构造 302 重定向响应
-    const response = {
-        status: 302,
-        headers: { 'Location': rawUrl }
-    };
-    
-    // 兼容不同 App 的环境退出方式
-    if (typeof $done !== "undefined") {
-        $done({ response });
-    } else {
-        $done({});
-    }
-} else {
-    $done({});
-}
+hostname = kk.weshine.im
