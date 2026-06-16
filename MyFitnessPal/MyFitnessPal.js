ae880f3cc20b292a034b42c2884132e9ab983b4c
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-06-16 22:28:26 +0800

    Delete

--- MyFitnessPal/MyFitnessPal.js
+++
@@ -1,6 +0,0 @@
-let body = JSON.parse($response.body);
-body.subscriptionEndDate = "2099-11-03T11:09:34.929Z",
-body.hasPremium = true,
-body.subscriptionStartDate = "2023-11-03T11:09:34.929Z"
-console.log(body);
-$done({body: JSON.stringify(body)});
